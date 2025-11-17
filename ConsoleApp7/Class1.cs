2c6aa51c2f7d46ccc2e1e4a8a5240b2d6be58a5a
bolot <bolot@bk.ru>
2025-11-17 09:26:27 +0600

    add WL C#

---
+++ ConsoleApp7/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Class1
+    {
+		private int myVar;
+
+		public int MyProperty
+		{
+			get { return myVar; }
+			set { myVar = value; }
+		}
+
+	}
+}
--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp7
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hello C#");
         }
     }
 }
